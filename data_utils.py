f9e118bae8668c35ca8e1eb5d7a99dbac801504d
Vivek Anand <vivek2000anand@gmail.com>
2021-10-25 18:03:09 -0400

    Added classification

--- data_utils.py
+++ data_utils.py
@@ -34,6 +34,38 @@ def load_all(test_num=100):
 			for i in arr[1:]:
 				test_data.append([u, int(i)])
 			line = fd.readline()
+	print(test_data[:3])
+	return train_data, test_data, user_num, item_num, train_mat
+
+
+
+def load_all_classification(test_num=100):
+	""" We load all the three file here to save time in each epoch. """
+	train_data = pd.read_csv(
+		config.train_rating, 
+		sep='\t', header=None, names=['user', 'item', 'rating'], 
+		usecols=[0, 1, 2, 3], dtype={0: np.int32, 1: np.int32, 2: np.int32})
+
+	user_num = train_data['user'].max() + 1
+	item_num = train_data['item'].max() + 1
+
+	train_data = train_data.values.tolist()
+
+	# load ratings as a dok matrix
+	train_mat = sp.dok_matrix((user_num, item_num), dtype=np.float32)
+	for x in train_data:
+		train_mat[x[0], x[1]] = x[2]
+
+	test_data = []
+	with open(config.test_negative, 'r') as fd:
+		line = fd.readline()
+		while line != None and line != '':
+			arr = line.split('\t')
+			u = eval(arr[0])[0]
+			test_data.append([u, eval(arr[0])[1]])
+			for i in arr[1:]:
+				test_data.append([u, int(i)])
+			line = fd.readline()
 	return train_data, test_data, user_num, item_num, train_mat
 
 
