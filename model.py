8e827a97aab14da445945ccbe80fa98c50523009
Vivek Anand <vivek2000anand@gmail.com>
2021-10-25 16:54:42 -0400

    Added classification

--- model.py
+++ model.py
@@ -45,7 +45,7 @@ class NCF(nn.Module):
 			self.predict_layer = nn.Linear(predict_size, 1)
 		else:
 			self.predict_layer = nn.Linear(predict_size, 6)
-			self.final_activation = nn.Softmax(6)
+			self.final_activation = nn.Softmax()
 
 		self._init_weight_()
 
