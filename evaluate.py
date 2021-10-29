64827e8e563ccf574d47ffc73d66b660067fba30
Vivek Anand <vivek2000anand@gmail.com>
2021-10-29 14:44:59 -0400

    Figuring out the accuracy

--- evaluate.py
+++ evaluate.py
@@ -45,5 +45,6 @@ def accuracy(model, test_loader):
 		prediction = model(user, item)
 		argmax_prediction = torch.argmax(prediction, dim=1)
 		correct += argmax_prediction.eq(label.view_as(argmax_prediction)).sum().item()
+	print("Length of test dataset is ", len(test_loader.dataset))
 	accuracy = 100* correct / len(test_loader.dataset)
 	return accuracy
