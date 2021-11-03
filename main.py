55411b2fc1b827c31c5e01be8e508651f56de64d
Vivek Anand <vivek2000anand@gmail.com>
2021-11-03 16:51:17 -0400

    adding print statements

--- main.py
+++ main.py
@@ -84,6 +84,7 @@ print("Making test loader")
 print("Length of test data before test loader is ", len(test_data))
 test_dataset = data_utils.NCFData(
 		test_data, item_num, train_mat, 0, True, args.classification)
+print("test dataset is false for training? ", test_dataset.is_training)
 train_loader = data.DataLoader(train_dataset,
 		batch_size=args.batch_size, shuffle=True, num_workers=4)
 test_loader = data.DataLoader(test_dataset,
