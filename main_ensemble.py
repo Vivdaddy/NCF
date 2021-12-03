b2be842651d6640eb53a4f4a8266ec6f035bc5d2
Vivek Anand <vivek2000anand@gmail.com>
2021-12-03 00:49:50 -0500

    adding testing changes

--- main_ensemble.py
+++ main_ensemble.py
@@ -91,6 +91,7 @@ print("Item num is ", item_num)
 # test_dataset = data_utils.NCFData(
 # 		test_data, item_num, test_labels, test_mat, 0, False, args.classification)
 test_dataset = data_utils.TestData(test_data, item_num)
+test_dataset.pad()
 padded_dataset = data_utils.PaddedData(test_data, item_num)
 # print("test dataset is false for training? ", test_dataset.is_training)
 train_loader = data.DataLoader(train_dataset,
